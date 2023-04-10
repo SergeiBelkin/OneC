fbb8bb32ca452f9e075cece114d3beec0d5923fb
SergeiBelkin <128678208+SergeiBelkin@users.noreply.github.com>
2023-04-10 13:50:07 +0300

    treesave

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter a");
+
+string entera = Console.ReadLine();
+int a = Convert.ToInt32(entera);
+
+Console.WriteLine("Enter b");
+
+string enterb = Console.ReadLine();
+int b = Convert.ToInt32(enterb);
+
+Console.WriteLine("Enter c");
+
+string enterc = Console.ReadLine();
+int c = Convert.ToInt32(enterc);
+int max = a;
+if (b>a) max = b;
+if (c>b) max = c;
+
+
+Console.Write(max);
