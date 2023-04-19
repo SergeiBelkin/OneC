0918167747186d4e00aead8d6ebf887b4bdf840c
SergeiBelkin <128678208+SergeiBelkin@users.noreply.github.com>
2023-04-19 16:48:51 +0300

    zad27

--- Program.cs
+++ Program.cs
@@ -1,20 +1,47 @@
-﻿Console.WriteLine("Enter a");
+﻿// Console.WriteLine ("Таблица умножения:");
+// for (int i = 10; i >=2; i--)
+// {
+//     Console.WriteLine($"умножение на {i}" );
+//     for (int u = 10; u >= 2; u-- )
+//     {
+        
+    
+//         Console.WriteLine($"{i}x{u}={u * i}");
+     
+//     }
+// } 
 
-string entera = Console.ReadLine();
-int a = Convert.ToInt32(entera);
+// Console.WriteLine("Введите слово:");
+// string txt = Console.ReadLine();
+// char[]chairA = txt.ToCharArray();
 
-Console.WriteLine("Enter b");
+// for( int c = txt.Length - 1; c >= 0; c--)
+// {
+//     // int i = c;
+//     Console.Write(txt[c]);
+    
+// }
 
-string enterb = Console.ReadLine();
-int b = Convert.ToInt32(enterb);
+// Задача 25:
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
-Console.WriteLine("Enter c");
+// Console.WriteLine("Введите число:");
+// string one = Console.ReadLine();
+// int two = Convert.ToInt32(one);
 
-string enterc = Console.ReadLine();
-int c = Convert.ToInt32(enterc);
-int max = a;
-if (b>a) max = b;
-if (c>b) max = c;
+// Console.WriteLine("Введите степень:");
+// string st = Console.ReadLine();
+// int three = Convert.ToInt32(st);
+// int q = two;
+
+//     for(int i = 1; i < three; i++)
+//     {
+//     q = q * two;
+
+//     }
 
 
-Console.Write(max);
+// Console.WriteLine(q);
+
